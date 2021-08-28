cb2609e670874274a7b8c795be8d3b40e817e20c
Lucas Nobre <lucasnobredev@outlook.com>
2021-08-28 12:09:12 -0300

    criação de entidade de produto

---
+++ SistemaDeVendas/SistemaDeVendas/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeVendas.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string Marca { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorDeCompra { get; set; }
+        public decimal ValorSugeridoDeVenda { get; set; }
+        public DateTime DataDeValidade { get; set; }
+    }
+}
