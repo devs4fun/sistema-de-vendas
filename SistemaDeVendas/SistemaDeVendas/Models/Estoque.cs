593fb0cbd4df494dd543a0dbadb1360716d9e21a
eneasdev <lucasmartins_eu@hotmail.com>
2021-09-27 12:08:03 -0300

    Nome da pasta de testes era Estoque, mudei para produto

---
+++ SistemaDeVendas/SistemaDeVendas/Models/Estoque.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeVendas.Models
+{
+    public class Estoque
+    {
+        public Estoque(HistoricoDeCompraRequest request)
+        {
+            Produto = request.Produto;
+            ValorDeCompra = request.ValorDeCompra;
+            DataDeCompra = request.DataDeCompra;
+            DataDeValidade = request.DataDeValidade;
+            QuantidadeDeProduto = request.QuantidadeDeProduto;
+        }
+        public int Id { get; set; }
+        public Produto Produto { get; private set; }
+        public decimal ValorDeCompra { get; private set; }
+        public decimal ValorSugerido => ValorDeCompra * 1.3M;
+        public DateTime DataDeCompra { get; private set; }
+        public DateTime DataDeValidade { get; private set; }
+        public int QuantidadeDeProduto { get; private set; }
+
+    }
+}
---
+++ SistemaDeVendas/SistemaDeVendas/Models/EstoqueRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SistemaDeVendas.Models
+{
+    public class HistoricoDeCompraRequest
+    {
+        public Produto Produto { get; set; }
+        public decimal ValorDeCompra { get; set; }
+        public DateTime DataDeCompra { get; set; }
+        public DateTime DataDeValidade { get; set; }
+        public int QuantidadeDeProduto { get; set; }
+    }
+}
